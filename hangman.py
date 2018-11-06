74d2b5fc2ca3531d08d84618f69b592f59ee1b1b
Matthew-HerandezL <34288485+Matthew-HerandezL@users.noreply.github.com>
2018-11-06 17:04:05 -0600

    HANGMAN.PY

--- hangman.py
+++ hangman.py
@@ -45,3 +45,24 @@ def draw_hangman(num_wrong_guesses):
         output = output + "\n |"
     output = output + "____\n\n"
     print(output)
+
+word = "Test"
+name = input("What is your name?").lower()
+wrong_guesses = 0
+user_guesses = []
+guess = 0
+max_wrong_guesses = 5
+print ("hello " + name + ", homie do you want to play the hangman game?")
+while wrong_guesses <= 5:
+    guess = input("What is your guess?").lower()
+    user_guesses.append(guess)
+    print("U have guessed the following... ",user_guesses)
+    if guess in word:
+        print("correct")
+    else:
+        wrong_guesses = wrong_guesses + 1
+        print("incorrect")
+    draw_hangman(wrong_guesses)
+
+if wrong_guesses == max_wrong_guesses:
+    print("Fail my guy")
